bd01ccdf17fe7f9fe8465188d9f0cd7a1f219684
Claudiu Cristea <clau.cristea@gmail.com>
2021-02-12 18:34:03 +0200

    ISAICP-6037: Initial code.

---
+++ tests/ChangelogCommandSimulationTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenEuropa\Changelog\Tests;
+
+use OpenEuropa\TaskRunner\Tests\CommandSimulationTest;
+
+/**
+ * Tests changelog commands.
+ */
+class ChangelogCommandSimulationTest extends CommandSimulationTest
+{
+    use HelperTestTrait;
+}
---
+++ tests/ChangelogCommandTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenEuropa\TaskRunner\Tests;
+
+use OpenEuropa\Changelog\Tests\HelperTestTrait;
+use OpenEuropa\TaskRunner\Commands\ChangelogCommands;
+use OpenEuropa\TaskRunner\TaskRunner;
+use Symfony\Component\Console\Input\StringInput;
+use Symfony\Component\Console\Output\NullOutput;
+
+/**
+ * Tests changelog commands.
+ */
+class ChangelogCommandTest extends AbstractTest
+{
+    use HelperTestTrait;
+
+    /**
+     * @param array $options
+     * @param string $expected
+     *
+     * @throws \Psr\Container\ContainerExceptionInterface
+     * @throws \Psr\Container\NotFoundExceptionInterface
+     *
+     * @dataProvider changelogDataProvider
+     */
+    public function testChangelogCommands(array $options, $expected): void
+    {
+        $runner = new TaskRunner(new StringInput(''), new NullOutput(), $this->getClassLoader());
+        /** @var ChangelogCommands $commands */
+        $commands = $runner->getCommands(ChangelogCommands::class);
+        $this->assertSame($expected, $commands->generateChangelog($options)->getCommand());
+    }
+
+    /**
+     * @return array
+     */
+    public function changelogDataProvider(): array
+    {
+        return $this->getFixtureContent('changelog.yml');
+    }
+}
